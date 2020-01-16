779bc0834c1e7059a5ce2d76e17f09d55c859a2d
plwp <Patrick.Prendergast@mh.org.au>
2020-01-16 14:57:01 +1100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pat_tools
 Toolkit for neuro-imaging data manipulation
+
+A collection of handy-dandy tools that Pat uses for neuro-imaging in python.
---
+++ pacs_example.py
@@ -0,0 +1,26 @@
+# You can replace the values in this file and take 'er for a test drive
+
+from pacs import *
+
+# These are the settings for your remote SCP, very important.
+# Depending on your PACS settings you may need to have your local aet registered with your PACS
+# That said, we're just using C_GET and C_FIND requests so maybe not.
+remote_scp = ScpSettings('REMOTESCP', 'remote.host', 104, local_aet='ANYSCP')
+# ScpSettings take remote AE Title, remote hostname, remote port, local AE title (optional)
+
+# Create a patient instance based on the patient id
+pat = Patient('1234567', remote_scp)
+print(pat)
+print('******************************')
+
+# Explore the studies
+studies = pat.find_studies()
+for study in studies:
+    print(str(study))
+    print('----------')
+    print(str(study.get_report()))
+    print('----------')
+
+# Now we'll try saving some images (nifti may fail if it's not a brain volume).
+studies[0].find_series()[0].save_dicom('./testdicom/')
+studies[0].find_series()[0].save_nifti('./test.nii.gz')
