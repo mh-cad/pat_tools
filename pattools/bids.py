58358b526ca4e3f6455326ab9c8be41cd42b6e73
plwp <pat@plwp.net>
2021-06-09 17:08:11 +1000

    Description file support

--- pattools/bids.py
+++ pattools/bids.py
@@ -1,8 +1,10 @@
 import os
 from enum import Enum
+import json
 
 SUBJECT_PREFIX = 'sub-'
 SESSION_PREFIX = 'ses-'
+BIDS_VERSION = 'v1.6.0'
 
 class ModalityType(Enum):
     ANATOMICAL = 'anat'
@@ -16,18 +18,191 @@ class ModalityType(Enum):
     def values():
         return [k.value for k in ModalityType]
 
-class Dataset:
+
+class SourceDatasetDescription:
+    URL: str = None
+    DOI: str = None
+    Version: str = None
+
+    def from_dict(dct: dict):
+        if dct is None:
+            return None
+        obj = SourceDatasetDescription()
+        obj.URL = dct.get('URL', None)
+        obj.DOI = dct.get('DOI', None)
+        obj.Version = dct.get('Version', None)
+
+        return obj
+
+    def to_dict(self):
+        return {
+            'URL': self.URL,
+            'DOI': self.DOI,
+            'Version': self.Version
+        }
+
+class DatasetDescription:
     path: str
+    parent = None
+    #BIDS defined fields
+    Name: str = None#REQUIRED
+    BIDSVersion: str = None#REQUIRED
+    HEDVersion: str = None#RECOMMENDED
+    DatasetType: str = None#RECOMMENDED [raw|derivative]
+    License: str = None#RECOMMENDED
+    Authors: [str] = []#OPTIONAL
+    Acknowledgements: str = None#OPTIONAL
+    HowToAcknowledge: str = None#OPTIONAL
+    Funding: [str] = []#OPTIONAL
+    EthicsApprovals: [str] = [] #OPTIONAL
+    ReferencesAndLinks: [str] = [] #OPTIONAL
+    DatasetDOI: str = None #OPTIONAL
+    #BIDS defined fields for derived types
+    GeneratedBy = []
+    SourceDatasets = []
+
+    def __init__(self, name):
+        self.BIDSVersion = BIDS_VERSION
+        self.Name = name
+    
+    def load(path):
+        with open(path, 'r') as f:
+            dd = DatasetDescription.from_dict(json.loads(f.read()))
+            dd.path = path
+            return dd
+
+    def save(self):
+        # Technically this an empty name is a name...
+        if self.Name == None:
+            self.Name = ''
+        if self.BIDSVersion == None:
+            self.BIDSVersion = BIDS_VERSION
+
+        with open(self.path, 'w') as f:
+            f.write(json.dumps(self.to_dict()))
+
+    def from_dict(dct):
+        obj = DatasetDescription(dct.get('Name', None))        
+        obj.BIDSVersion = dct.get('BIDSVersion', None)
+        obj.HEDVersion = dct.get('HEDVersion', None)
+        obj.DatasetType = dct.get('DatasetType', None)
+        obj.License = dct.get('License', None)
+        obj.Authors = dct.get('Authors', None)
+        obj.Acknowledgements = dct.get('Acknowledgements', None)
+        obj.HowToAcknowledge = dct.get('HowToAcknowledge', None)
+        obj.EthicsApprovals = dct.get('EthicsApprovals', None)
+        obj.ReferencesAndLinks = dct.get('ReferencesAndLinks', None)
+        obj.DatasetDOI = dct.get('DatasetDOI', None)
+        if 'GeneratedBy' in dct:
+            obj.GeneratedBy = []
+            for d in dct['GeneratedBy']:
+                gb = GeneratedByDescription.from_dict(d)
+                gb.parent = obj
+                obj.GeneratedBy.append(gb)
+        if 'SourceDatasets' in dct:
+            obj.SourceDatasets = []
+            for d in dct['SourceDatasets']:
+                sd = SourceDatasetDescription.from_dict(d)
+                sd.parent = obj
+                obj.SourceDatasets.append(sd)
+        return obj
+
+    def to_dict(self):
+        print(self.HEDVersion)
+        return {
+            'Name': self.Name,
+            'BIDSVersion': self.BIDSVersion,
+            'HEDVersion': self.HEDVersion,
+            'DatasetType': self.DatasetType,
+            'License': self.License,
+            'Authors': json.dumps(self.Authors),
+            'Acknowledgements': self.Acknowledgements,
+            'HowToAcknowledge': self.HowToAcknowledge,
+            'Funding': json.dumps(self.Funding),
+            'EthicsApprovals': json.dumps(self.EthicsApprovals),
+            'ReferencesAndLinks': json.dumps(self.ReferencesAndLinks),
+            'DatasetDOI': self.DatasetDOI,
+            'GeneratedBy': [o.to_dict() for o in self.GeneratedBy],
+            'SourceDatasets': [o.to_dict() for o in self.SourceDatasets]
+        }
+
+class Dataset:
+    parent = None
+    path: str
+    description: DatasetDescription
     
     def __init__(self, path):
         self.path = path
+        try:
+            self.description = DatasetDescription.load(os.path.join(path, 'dataset_description.json'))
+        except:
+            self.description = DatasetDescription(os.path.split(path)[1])
+            self.description.path = os.path.join(path, 'dataset_description.json')
+            self.description.save()
     
     def subjects(self):
         root, folders, _ = next(os.walk(self.path))
         # Subject folders are "sub-<label>".
         return [Subject(os.path.join(root, f), self) for f in folders if f.startswith(SUBJECT_PREFIX)]
 
+class ContainerDescription:
+    parent = None
+    #BIDS defined fields
+    Type: str #e.g. docker
+    Tag: str #e.g. plwp/pattools
+    URI: str 
+
+    def from_dict(dct: dict):
+        if dct is None:
+            return None
+        obj = ContainerDescription()
+        obj.Type = dct.get('Type', None)
+        obj.Tag = dct.get('Tag', None)
+        obj.URI = dct.get('URI', None)
+        return obj
+    
+    def to_dict(self):
+        return {
+            'Type': self.Type,
+            'Tag': self.Tag,
+            'URI': self.URI
+        }     
+
+class GeneratedByDescription:
+    parent: DatasetDescription
+    #BIDS defined fields
+    Name: str = None #REQUIRED
+    Version: str = None #RECOMMENDED
+    Description: str = None #OPTIONAL
+    CodeURL: str = None #OPTIONAL
+    Container: ContainerDescription = None #OPTIONAL
+
+    def from_dict(dct: dict):
+        if dct is None:
+            return None
+        obj = GeneratedByDescription()
+        obj.Name = dct.get('Name', None)
+        obj.Version = dct.get('Version', None)
+        obj.Description = dct.get('Description', None)
+        obj.CodeURL = dct.get('CodeURL', None)
+        if 'Container' in dct:
+            obj.Container = ContainerDescription.from_dict(dct['Container'])
+            if obj.Container != None:
+                obj.Container.parent = obj
+        return obj
+
+    def to_dict(self):
+        return {
+            'Name': self.Name,
+            'Version': self.Version,
+            'Description': self.Description,
+            'CodeURL': self.CodeURL,
+            'Container': None if self.Container == None else self.Container.to_dict()
+        }
+   
+
 class Subject:
+    '''Represents a single subject within the dataset'''
     path: str
     label: str
     parent: Dataset
@@ -41,6 +216,8 @@ class Subject:
         self.label = directory[4:]
 
     def sessions(self):
+        '''Return all the sessions for this patient. If there are no session folders 
+        the patient folder will be treated as a single session'''
         _, folders, _ = next(os.walk(self.path))
         session_folders = [f for f in folders if f.startswith(SESSION_PREFIX)]
         if len(session_folders) > 0:
@@ -51,6 +228,7 @@ class Subject:
 
 
 class Session:
+    '''Represents a single session folder'''
     path: str
     label: str
     parent: Subject
@@ -67,15 +245,26 @@ class Session:
             self.label = None # Empty label may be useful for parsing filenames
     
     def modalities(self):
+        '''Return all the modalities for this session'''
         _, folders, _ = next(os.walk(self.path))
         [Modality(os.path.join(f, self.path), self) for f in folders if f in ModalityType.values()]
 
+    def get_modality(self, modality_type: ModalityType):
+        '''Return the modality based on type. May return None.'''
+        modality_path = os.path.join(self.path, modality_type.value)
+        if os.path.exists(modality_path):
+            return Modality(modality_path, self)
+        # TODO: There may be a better error here.
+        return None
+
 class Modality:
     path: str
+    parent: Session
     modality_type: ModalityType
 
     def __init__(self, path:str, parent:Session=None):
         self.path = path
+        self.parent = parent
         modality_type = ModalityType(path.split(self.path)[1])
 
 
--- pattools/test/test_bids.py
+++ pattools/test/test_bids.py
@@ -2,9 +2,39 @@ import unittest
 from tempfile import TemporaryDirectory
 from pattools.bids import *
 import os
+import json
 
 class TestDataset(unittest.TestCase):
     
+    test_data = '''
+    {
+        "Name": "FMRIPREP Outputs",
+        "BIDSVersion": "1.4.0",
+        "DatasetType": "derivative",
+        "GeneratedBy": [
+            {
+            "Name": "fmriprep",
+            "Version": "1.4.1",
+            "Container": {
+                "Type": "docker",
+                "Tag": "poldracklab/fmriprep:1.4.1"
+                }
+            },
+            {
+            "Name": "Manual",
+            "Description": "Re-added RepetitionTime metadata to bold.json files"
+            }
+        ],
+        "SourceDatasets": [
+            {
+            "DOI": "doi:10.18112/openneuro.ds000114.v1.0.1",
+            "URL": "https://openneuro.org/datasets/ds000114/versions/1.0.1",
+            "Version": "1.0.1"
+            }
+        ]
+        }
+    '''
+
     def setUp(self):
         pass
 
@@ -24,3 +54,18 @@ class TestDataset(unittest.TestCase):
             assert(subs[1].label == '02')
             assert(subs[0].sessions()[0].label == '01')
             assert(subs[1].sessions()[0].label == None)
+
+    def test_description(self):
+        ddct = json.loads(self.test_data)
+        assert(ddct['Name'] == 'FMRIPREP Outputs')
+        dd = DatasetDescription.from_dict(ddct)
+        dct = dd.to_dict()
+        assert(dct['Name'] == 'FMRIPREP Outputs')
+
+        assert(dd.SourceDatasets[0].DOI == "doi:10.18112/openneuro.ds000114.v1.0.1")
+
+        with TemporaryDirectory() as tmpdir:
+            dd.path = os.path.join(tmpdir, 'dataset_description.json')
+            dd.save()
+            loaded = DatasetDescription.load(dd.path)
+            assert(loaded.GeneratedBy[1].Name == "Manual")
